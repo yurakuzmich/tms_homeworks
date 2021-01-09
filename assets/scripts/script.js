e54e8b1093d53500aa4d08feb5933559fd09cc4e
Yury Kuzmich <yurakuzmich@gmail.com>
2021-01-09 13:55:41 +0300

    feat: add list toggle

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,28 @@
+const buttonShow = document.getElementById("button-show");
+const buttonHide = document.getElementById("button-hide");
+const list = document.querySelector(".list-container");
 
+const buttons = [buttonShow, buttonHide];
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', toggleList);
+});
+
+function toggleList(e) {
+    
+    e.target.dataset.action === 'hide' ? hideList() : showList();
+    
+    buttons.forEach(button => {
+        button === e.target ? button.disabled = true : button.disabled = false;
+        button.classList.toggle('active');
+    });
+}
+
+function showList() {
+    list.classList.remove('hide');
+}
+
+function hideList() {
+    list.classList.add('hide');
+}
